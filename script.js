6593cc53170669cf7c1d2ce342f06319cffbe90a
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-02-26 18:30:46 +0530

    my website project

---
+++ script.js
@@ -0,0 +1,11 @@
+// javascript
+titleText = document.getElementsByTagName("h1")[0];
+contentText = document.getElementsByTagName("h3")[0];
+
+titleText.addEventListener("mouseover", () => {
+  alert("Welcome to My Website?");
+});
+
+contentText.addEventListener("mouseover", () => {
+  alert("Hi i am shubham. thanks for visiting my website");
+});
